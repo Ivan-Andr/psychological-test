c56e777306948018870b6b7162b1c1f82811c5b0
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-01-31 10:00:43 +0500

    added buttons (without styling)

--- src/table/table.js
+++ src/table/table.js
@@ -1,25 +1,5 @@
 "use strict";
-/*
-const redArray = [];
-const blackArray = [];
-let num = Math.trunc(Math.random() * 25) + 1;
-for (let i = 1; redArray.length; i++) {
-  if (redArray.includes(num) === false) {
-    redArray.push(num);
-    num = Math.trunc(Math.random() * 25 + 1);
-  }
-}
-console.log(redArray);*/
-/* const arr = [
-  [3, 10, 2, 1, 24, 22, 4],
-  [8, 8, 23, 17, 10, 16, 14],
-  [19, 13, 5, 22, 13, 7, 6],
-  [23, 14, 25, 9, 19, 21, 21],
-  [12, 18, 2, 15, 1, 5, 11],
-  [18, 7, 9, 12, 20, 16, 17],
-  [24, 6, 15, 20, 3, 4, 11],
-];
-*/
+
 const tableArr = [
   3, 10, 2, 1, 24, 22, 4, 8, 8, 23, 17, 10, 16, 14, 19, 13, 5, 22, 13, 7, 6, 23,
   14, 25, 9, 19, 21, 21, 12, 18, 2, 15, 1, 5, 11, 18, 7, 9, 12, 20, 16, 17, 24,
@@ -29,7 +9,14 @@ const redArray = [
   2, 5, 6, 7, 10, 11, 13, 15, 16, 19, 21, 25, 27, 28, 29, 31, 32, 37, 40, 42,
   43, 45, 46, 48,
 ];
+const tableSpace = document.createElement("article");
+document.body.appendChild(tableSpace);
+tableSpace.className = "tbl--space";
+
 export function generateTable() {
+  const tableSpace = document.createElement("article");
+  document.body.append(tableSpace);
+  tableSpace.className = "tbl--space";
   // creates a <table> element and a <tbody> element
   const tbl = document.createElement("table");
   const tblBody = document.createElement("tbody");
@@ -45,6 +32,8 @@ export function generateTable() {
       // the end of the table row
       let k = i * 7 + j;
       const cell = document.createElement("td");
+      cell.setAttribute("id", `table-cell-${k}`);
+      //cell.setAttribute("id", "cellTest");
       const cellText = document.createTextNode(tableArr[k]);
       cell.appendChild(cellText);
       if (redArray.includes(k)) {
@@ -62,7 +51,26 @@ export function generateTable() {
   // put the <tbody> in the <table>
   tbl.appendChild(tblBody);
   // appends <table> into <body>
-  document.body.appendChild(tbl);
+  tableSpace.appendChild(tbl);
   // sets the border attribute of tbl to '2'
   tbl.setAttribute("border", "1");
 }
+const rightSpace = document.createElement("aside");
+document.body.appendChild(rightSpace);
+rightSpace.className = "ui--space";
+const btnStart = document.createElement("button");
+document.body.appendChild(btnStart);
+btnStart.className = "btn";
+btnStart.innerHTML = "Начать тест";
+const btnFinish = document.createElement("button");
+document.body.appendChild(btnFinish);
+btnFinish.innerHTML = "Завершить тест";
+btnFinish.className = "btn";
+rightSpace.appendChild(btnStart);
+rightSpace.appendChild(btnFinish);
+btnStart.addEventListener("click", function () {
+  console.log("Test started");
+});
+btnFinish.addEventListener("click", function () {
+  console.log("Test finished");
+});
