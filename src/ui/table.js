4a8d7b08c0ee6194a06a4b4a48d2ef969a852d6f
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-02-04 11:27:19 +0500

    some changes according to comments

--- src/ui/table.js
+++ src/ui/table.js
@@ -30,20 +30,28 @@ export const table = () => {
       cell.setAttribute("id", `table-cell-${k}`);
       //cell.setAttribute("id", "cellTest");
       const cellText = document.createTextNode(tableArr[k]);
-
       cell.appendChild(cellText);
+
+      // if (redArray.includes(k)) {
+      //   cell.className = "table_cell_red";
+
+      //   cell.addEventListener("click", function () {
+      //     console.log(`${tableArr[k]} red is clicked`);
+      //   });
+      // } else {
+      //   cell.className = "table_cell_black";
+      //   cell.addEventListener("click", function () {
+      //     console.log(`${tableArr[k]} black is clicked`);
+      //   });
+      // }
       if (redArray.includes(k)) {
         cell.className = "table_cell_red";
-
-        cell.addEventListener("click", function () {
-          console.log(`${tableArr[k]} red is clicked`);
-        });
       } else {
         cell.className = "table_cell_black";
-        cell.addEventListener("click", function () {
-          console.log(`${tableArr[k]} black is clicked`);
-        });
       }
+      cell.addEventListener("click", (event) => {
+        console.log(`${event.target.innerText} ${event.target.className}`);
+      });
       row.appendChild(cell);
     }
 
