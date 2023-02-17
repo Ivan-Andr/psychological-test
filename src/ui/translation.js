8d58daa711753c12e1c501d0b4c715f441cb3d72
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-02-17 23:19:42 +0500

    add next button and instruction to stages 1,2

--- src/ui/translation.js
+++ src/ui/translation.js
@@ -9,10 +9,20 @@ export const langArr = {
     uz: "❗Testni o'tkazish bo'yicha ko'rsatmalar",
     en: "❗Instructions for the test",
   },
+  "instruction-stage1": {
+    ru: "Задание 1. Сейчас  Вы  должны  находить  числа только черного    цвета  в  порядке  возрастания,  начиная  с  1  и  заканчивая  25.    Необходимо  стараться  работать быстро и безошибочно.  Если задача  понятна, попробуйте выполнить ее на таблице.",
+    uz: " Vazifa 1. Endi siz 1 dan boshlanib, 25 bilan tugaydigan o'sish tartibida faqat qora raqamlarni topishingiz kerak. Siz tez va aniq ishlashga harakat qilishingiz kerak. Agar vazifa aniq bo'lsa, uni jadvalda bajarishga harakat qiling.",
+    en: "Task 1. Now you have to find only black numbers in ascending order, starting from 1 and ending with 25. It is necessary to try to work quickly and accurately. If the task is clear, try to perform it on the table.",
+  },
+  "instruction-stage2": {
+    ru: "Задание 2. Сейчас  Вы  должны находить числа только красного цвета в порядке убывания, начиная с 24 и заканчивая 1",
+    uz: "Vazifa 2. Endi siz 24 dan boshlanib, 1 bilan tugaydigan kamayish tartibida faqat qizil raqamlarni topishingiz kerak.",
+    en: "Task 1. Now you have to find only red numbers in descending order, starting from 24 and ending with 1.",
+  },
   "instruction-stage3": {
-    ru: " Задание 3. Сейчас Вы должны одновременно находить и черные, и красные числа, чередуя их в следующем порядке: 1 черная - 24 красная, 2 черная - 23 красная, 3 черная - 22 красная и т.д., т.е. цифры черного цвета нарастают (как в первом задании), а цифры красного цвета убывают (как во втором задании). Закончить Вы должны самой маленькой красной и самой большой черной цифрой, т.е. 1 красная  - 25 черная. Задание необходимо довести до конца, не прерывая его.",
-    uz: " Vazifa 3. Endi siz bir vaqtning o'zida qora va qizil ranglarni topishingiz kerak. raqamlar, ularni quyidagi tartibda almashtirish: 1 qora-24 qizil, 2 qora - 23 qizil, 3 qora-22 qizil va boshqalar, ya'ni qora raqamlar ular o'sadi (birinchi vazifada bo'lgani kabi) va qizil raqamlar kamayadi (masalan ikkinchi topshiriq). Siz eng kichik qizilni va o'zingizni tugatishingiz kerak katta qora raqam, ya'ni. - 25 ch. Vazifani quyidagilarga etkazish kerak oxirini to'xtatmasdan.",
-    en: " Task 3. Now you have to find both black and red  at the same time, alternating them in the following order: 1 black - 24 red, 2 black - 23 red, 3 black - 22 red, etc. Black numbers increase (as in the first task), and the red numbers decrease (as in the second task). You must finish with the smallest red and the largest black number, i.e. 1 red - 25 black. The task must be brought to the end without interrupting it.",
+    ru: "Задание 3. Сейчас Вы должны одновременно находить и черные, и красные числа, чередуя их в следующем порядке: 1 черная - 24 красная, 2 черная - 23 красная, 3 черная - 22 красная и т.д., т.е. цифры черного цвета нарастают (как в первом задании), а цифры красного цвета убывают (как во втором задании). Закончить Вы должны самой маленькой красной и самой большой черной цифрой, т.е. 1 красная  - 25 черная. Задание необходимо довести до конца, не прерывая его.",
+    uz: "Vazifa 3. Endi siz bir vaqtning o'zida qora va qizil ranglarni topishingiz kerak. raqamlar, ularni quyidagi tartibda almashtirish: 1 qora-24 qizil, 2 qora - 23 qizil, 3 qora-22 qizil va boshqalar, ya'ni qora raqamlar ular o'sadi (birinchi vazifada bo'lgani kabi) va qizil raqamlar kamayadi (masalan ikkinchi topshiriq). Siz eng kichik qizilni va o'zingizni tugatishingiz kerak katta qora raqam, ya'ni. - 25 ch. Vazifani quyidagilarga etkazish kerak oxirini to'xtatmasdan.",
+    en: "Task 3. Now you have to find both black and red  at the same time, alternating them in the following order: 1 black - 24 red, 2 black - 23 red, 3 black - 22 red, etc. Black numbers increase (as in the first task), and the red numbers decrease (as in the second task). You must finish with the smallest red and the largest black number, i.e. 1 red - 25 black. The task must be brought to the end without interrupting it.",
   },
   "form-header": {
     ru: "📝 Введите свои данные",
@@ -73,5 +83,10 @@ export const langArr = {
     ru: "Инструкция к тесту",
     uz: "Testni ko'rsatmalar",
     en: "Instructions for the test",
+  },
+  "btn--next": {
+    ru: "К следующему заданию",
+    uz: "Keyingi vazifa",
+    en: "Next task",
   },
 };
