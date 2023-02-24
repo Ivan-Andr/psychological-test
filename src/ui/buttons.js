45fc1b0a17f4a720dcf390075ceccf4aff6d05a4
Ivan-Andr <you@example.comlevchenko-ivan@mail.ru>
2023-02-24 15:47:28 +0500

    add finish test button

--- src/ui/buttons.js
+++ src/ui/buttons.js
@@ -4,6 +4,7 @@ import { timer } from './timer.js';
 import { form } from './form.js';
 const btnStart = document.querySelector('#btn--start');
 const btnFinish = document.querySelector('#btn--finish');
+const btnStop = document.querySelector('#btn--stop');
 const btnInstruction = document.querySelector('#btn--instruction');
 const btnCloseInstruction = document.querySelector('.close-modal');
 export const btnOpenForm = document.querySelector('.login-button');
@@ -16,7 +17,6 @@ const { openModalForm, closeModalForm } = form();
 export const startApp = () => {
   btnOpenForm.addEventListener('click', function () {
     openModalForm();
-    console.log('Open form');
   });
   btnCloseForm.addEventListener('click', closeModalForm);
 };
@@ -28,7 +28,7 @@ export const buttons = () => {
     startTestTimer();
   });
 
-  btnFinish.addEventListener('click', function () {
+  btnStop.addEventListener('click', function () {
     stopTestTimer();
     btnInstruction.removeAttribute('disabled');
     btnInstruction.style.pointerEvents = 'auto';
@@ -38,5 +38,14 @@ export const buttons = () => {
     openModal();
   });
 
-  btnCloseInstruction.addEventListener('click', closeModal);
+  btnFinish.addEventListener('click', function () {
+    if (
+      window.confirm(
+        'Вы уверены, что хотите завешить тест?\nТекущий результат будет потерян!'
+      )
+    ) {
+      location.reload();
+    }
+    btnCloseInstruction.addEventListener('click', closeModal);
+  });
 };
--- src/ui/lang.js
+++ src/ui/lang.js
@@ -1,12 +1,11 @@
-"use strict";
-import { langArr } from "./translation.js";
-export const langSelect = document.querySelector(".change-lang");
-const languages = ["ru", "en", "uz"];
-langSelect.addEventListener("change", changeURLLanguage);
+'use strict';
+import { langArr } from './translation.js';
+export const langSelect = document.querySelector('.change-lang');
+const languages = ['ru', 'en', 'uz'];
+langSelect.addEventListener('change', changeURLLanguage);
 function changeURLLanguage() {
   let lang = langSelect.value;
-  location.href = window.location.pathname + "#" + lang;
-  //location.reload();
+  location.href = window.location.pathname + '#' + lang;
   changeLanguage();
 }
 
@@ -15,14 +14,14 @@ export function changeLanguage() {
   hash = hash.substring(1);
   console.log(hash);
   if (!languages.includes(hash)) {
-    location.href = window.location.pathname + "#ru";
+    location.href = window.location.pathname + '#ru';
     location.reload();
   }
   langSelect.value = hash;
   for (let key in langArr) {
     let element = document.querySelector(`#${key}`);
     if (langArr[key][hash] === undefined) {
-      langArr[key][hash] = langArr[key]["ru"];
+      langArr[key][hash] = langArr[key]['ru'];
     }
     if (element) {
       element.innerHTML = langArr[key][hash];
